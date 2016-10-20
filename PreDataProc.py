d8d28fa5a93f53e3c8fcc2df0fb05a69cdd97905
Dynamitelaw <noobeater122333444455555@gmail.com>
2016-10-20 19:53:00 -0400

    Update PreDataProc.py

--- PreDataProc.py
+++ PreDataProc.py
@@ -1,26 +1,154 @@
 '''
 Dynamitelaw
-
 Processes raw stock data for neural network intput. 
-Adds 2 day slope, 5 day slope, and 30 day standard deviation.
-
+Adds 2 day slope, 5 day slope, 30 day standard deviation, optimal buy and sell dates, and desired network outputs.
 '''
 
 import numpy as np
 
-def optimalbuy(array)
+
+def optimalbuy(array):
     '''
-    Makes an array marking the optimal buy and sell dates. 
+    Makes an array marking the optimal buy and sell dates, as well as desired outputs for Network Level 1. 
     1 for buy, -1 for sell, 0 OTW
     '''
     
-    state = 0
-    k = int(data.shape[0])
+    state = 0       #state = 0 if no stock is owned
+    k = int(array.shape[0])
+    opt = np.zeros((k,1))
     
-    for i in range(k,0,-1):
-        if array[i,1] < array[i-1,1] and array[i,1] < array[i-5,1]
+    for i in range(k-8,6,-1):       #edits array opt to show optimal buying and selling times
+        
+        if state == 0:
+            if array[i,0] < array[i-1,0] and array[i,0] < array[i-7,0]:
+                idx = np.argmin(array[i-7:i+7,0])       #index of local minimum price
+                opt [idx+i-7] = 1
+                state = 1
+        
+        if state == 1:
+            if array[i,0] > array[i-1,0] and array[i,0] > array[i-3,0]:
+                idx = np.argmax(array[i-3:i+3,0])       #index of local maximum price
+                opt [idx+i-3] = -1
+                state = 0
+
+    bought = 0
+    for i in range(k-1,0,-1):
+        #corrects for double buys
+        if opt[i] == 1 and bought == 1:
+            opt[i] = 0
+        if opt[i] == 1 and bought == 0:
+            bought = 1      
+        #corrects for double sells
+        if opt[i] == -1 and bought == 0:
+            opt[i] = 0
+        if opt[i] == -1 and bought == 1:
+            bought = 0
+        
+                
+    desire = np.zeros((k,2))
+    
+    output = np.hstack((opt,desire))
+    
+    for i in range(0,k,1):      #creates columns of desired outputs for Network Level 1
+        #buying
+        if output[i,0] == 1:
+            output[i,1] = 1
+        try:
+            if (output[i-1,0] == 1 or output[i+1,0] == 1) and i >= 1 and i <= k-2:
+                output[i,1] = 0.8
+        except:
+            pass
+        try:
+            if (output[i-2,0] == 1 or output[i+2,0] == 1) and i >= 2 and i <= k-3:
+                if output[i,1] < 0.8 and output[i,1] > 0:       #averages buy overlap
+                    output[i,1] = (output[i,1] + 0.5)/2
+                if output[i,1] == 0:
+                    output[i,1] = 0.5
+        except:
+            pass
+        try:
+            if (output[i-3,0] == 1 or output[i+3,0] == 1) and i >= 3 and i <= k-4:
+                if output[i,1] < 0.8 and output[i,1] > 0:       #averages buy overlap
+                    output[i,1] = (output[i,1] + 0.1)/2
+                if output[i,1] == 0:
+                    output[i,1] = 0.1
+        except:
+            pass
+        
+        #selling
+        if output[i,0] == -1:
+            output[i,2] = -1
+        try:
+            if (output[i-1,0] == -1 or output[i+1,0] == -1) and i >= 1 and i <= k-2:
+                output[i,2] = -0.8
+        except:
+            pass
+        try:
+            if (output[i-2,0] == -1 or output[i+2,0] == -1) and i >= 2 and i <= k-3:
+                if output[i,2] > -0.8 and output[i,2] < 0:      #averages sell overlap
+                    output[i,2] = (output[i,2] - 0.5)/2
+                if output[i,2] == 0:
+                    output[i,2] = -0.5          
+        except:
+            pass
+        try:
+            if (output[i-3,0] == -1 or output[i+3,0] == -1) and i >= 3 and i <= k-4:
+                if output[i,2] > -0.8 and output[i,2] < 0:      #averages sell overlap
+                    output[i,2] = (output[i,2] - 0.1)/2
+                if output[i,2] == 0:
+                    output[i,2] = -0.1       
+        except:
+            pass
+     
+    
+    return output
     
 
+def ProfitSpeed(array):
+    '''
+    Generates a column of profit speeds (%profit/dt). Desired output of Network Level 2
+    Array must have already gone through optimalbuy(). 
+    '''
+    
+    k = int(array.shape[0])
+    ps = np.zeros((k,1))
+    
+    for i in range(k-1,0,-1):
+        if array[i,9] == 1:     #if date i is an optimal buy date
+            sindx = i - 1
+            z = True
+            while z==True:
+                sindx = sindx - 1
+                if sindx > 0 or sindx == 0:
+                    if array[sindx,9] == -1:        #sindx = index of next optimal sell day
+                        z = False
+                if sindx < 0:
+                    z = False
+                    
+            if sindx > 0 or sindx == 0:
+                Pspeed = (array[sindx,0] - array[i,0]) / ((array[i,0]) * (i - sindx))       #profit/(time*price at buy in)
+                ps[i] = Pspeed 
+
+                #finds profit speed before optimal buy date
+                z = True
+                d = 0
+                while z == True:
+                    d += 1
+                    if (i+d)<k and array[i+d,9] == 0:
+                        Pspeedp1 = (array[sindx,0] - array[i+d,0]) / ((array[i+d,0]) * (i+d - sindx))
+                        ps[i+d] = Pspeedp1 
+                    else:
+                        z = False
+               
+                #finds profit speed after optimal buy date
+                for r in range(i-1,sindx,-1):
+                    Pspeedr = (array[sindx,0] - array[r,0]) / ((array[r,0]) * (r - sindx))
+                    ps[r] = Pspeedr
+            
+    
+    return ps
+  
+    
 def process(ticker):
 
     '''
@@ -39,13 +167,13 @@ def process(ticker):
     zeros = np.zeros((k,3))
     data = np.hstack((data,zeros))
     
-    for i in range(0,k-1,1):        #6th row is the 2 day slope 
+    for i in range(0,k-1,1):        #6th column is the 2 day slope 
         data[i,6] = data[i,0] - data[i+1,0]
 
-    for i in range(0,k-4,1):        #7th row is the 5 day slope
+    for i in range(0,k-4,1):        #7th column is the 5 day slope
         data[i,7] = data[i,0] - data[i+4,0]
     
-    for i in range(0,k-30,1):       #8th row is the month's standard deviation
+    for i in range(0,k-30,1):       #8th column is the month's standard deviation
         data[i,8] = np.std(data[i:i+30,0])
 
         
@@ -65,24 +193,65 @@ def process(ticker):
                 arow = np.array(r[0])  
                 dates = np.vstack((dates,arow))
     
- 
+                
+    optimal = optimalbuy(data)
+    data = np.hstack((data,optimal))        #9th column optimal buy and sell dates, 10th column desired Level 1 Outputs
+    sprofit = ProfitSpeed(data)
+    
+    indx = 0
+    while True:     #finds index of most recent sell date
+        if sprofit[indx,0] >0:
+            indx = indx - 1
+            break
+        else:
+            indx += 1
+    
+    data = np.hstack((data,sprofit))        #10th column is the profit speed for that day
+    
+    data = data[indx:k-31,:]     #truncates unprocessed data at beggining of date range and the end week of date range
+    dates = dates[indx:k-31,:]       #truncates dates list
+    newlen = int(data.shape[0])
+    
     savepath = './Data/PcsData/' + ticker + '.csv'
     fout = open(savepath,'w')
-    for i in range (0,k,1):     #writes processed data to new file
+    header = 'Date, Open, High, Low, Close, Volume, Adj Close, 2 Day Slope, 5 Day Slope, Standard Dev, Optimal Dates, Desired Level 1 Out Buy, Desired Level 1 Out Sell, Profit Speed'
+    fout.write(header + '\n')
+    for i in range (0,newlen,1):     #writes processed data to new file
         line = [dates[i,0]]
         dline = data[i,:].tolist()
         line.extend(dline)
         fout.write(str(line).strip("[]") + '\n')
     
     fout.close()
+  
     
+def main():
+    '''
+    Iterates process through all tickers on list
+    '''
+    
+    ticks = open('./Data/ListOfTickerSymbols.csv','r')
+    
+    length = len(ticks.readlines())
+    row = 0
+    for l in ticks:     #process all stocks on the ticker list
+        if row == 0:
+            row += 1
+        else:
+            row += 1
+            ticker = str(l).rstrip()
+            try:
+                process(ticker)
+                print ('Proccessed' + ticker + ': ' + str(int((100*row)/length)) + '% done')
+            except:
+                print ('Error Processing Stock' + ticker)
+    
+    
+    ticks.close()
 
-ticks = open('./Data/ListOfTickerSymbols.csv','r')
 
-row = 0
-for l in ticks:     #process all stocks on the ticker list
-    if row == 0:
-        row += 1
-    else:
-        ticker = str(l).rstrip()
-        process(ticker)
+#-------------------------------------------------------------------------------------------
+
+main()      #main isn't working currently: there's some issue reading the ticker list, but no error is thrown
+
+#process('1HTA.SG')
