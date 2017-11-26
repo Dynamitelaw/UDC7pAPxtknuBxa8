c6145fe790940f4216193af25456e26ec1a36bb4
ilPesce41 <cole.hill41@gmail.com>
2017-11-26 17:16:35 -0500

    Add files via upload

--- NearestNeighborTester.py
+++ NearestNeighborTester.py
@@ -7,13 +7,15 @@ Tests and optimizes the NearestNeighbor Algorithm.
 import numpy as np
 import sys
 import os
-import NearestNeighborBackend as knb
+from scipy import spatial
+
 from scipy import stats
 import warnings
 import time
 import math
 import multiprocessing as mp
 from usefulThings import printLocation
+
 
 
 def NearestNeighborTester(ticker,bs,fields, daterange = None, points = 5, distanceMetric = 'seuclidean', sampleLog = False, ignoreWarnings = True, progressHeader = '', dayBackValue = 0, overideDirectory = False, silencePrint = False):
@@ -50,7 +52,7 @@ def NearestNeighborTester(ticker,bs,fields, daterange = None, points = 5, distan
 
     try:
         #Generates data set of stock
-        dataSet = knb.GenerateNearestData(ticker,bs, fields, daterange, OutputMultiple = False, includeOptimalDates = True, overideDirectory = overideDirectory)
+        dataSet = GenerateNearestData(ticker,bs, fields, daterange, OutputMultiple = False, includeOptimalDates = True, overideDirectory = overideDirectory)
         dataPoints = dataSet[0]
         outputs = dataSet[1]
         optimals = dataSet[2]
@@ -84,10 +86,10 @@ def NearestNeighborTester(ticker,bs,fields, daterange = None, points = 5, distan
                 if ignoreWarnings:         #supresses warnings
                     with warnings.catch_warnings():
                         warnings.simplefilter("ignore")
-                        prediction = knb.NearestPredict(sourcePoint, historicalDataInput, hOutputs, points, distanceMetric)        #predicts output
+                        prediction = NearestPredict(sourcePoint, historicalDataInput, hOutputs, points, distanceMetric)        #predicts output
                         predictionSet[i] = prediction
                 else:
-                    prediction = knb.NearestPredict(sourcePoint, historicalDataInput, hOutputs, points, distanceMetric)        #predicts output
+                    prediction = NearestPredict(sourcePoint, historicalDataInput, hOutputs, points, distanceMetric)        #predicts output
                     predictionSet[i] = prediction
 
             correlation = stats.pearsonr(predictionSet,outputs)[0]
@@ -283,7 +285,16 @@ def NNOptimizer(runTimeName = '', shortenProgress = False, PrinterCarriageNumber
                             cout.close()
                             printLocation (text = header + '(*SVD)', x = PrinterCarriageNumber,y = 25)
 
-                    corAvg = corSum / stocksTried
+                    if (stocksTried > 0):
+                        corAvg = corSum / stocksTried
+                    else:       #if metric invalid updates checkpoint and moves on with its life
+                        savepath = runTimeName + 'NNOptimizerCheckpoint.txt'
+                        cout = open(savepath,'w')
+                        cout.write(str(metricMin) + '\n' + str((m + 1)) + '\n' + str(metricMax) + '\n' + str(fieldPermMin) + '\n' + str(fieldPermMin) + '\n' + str(fieldPermMax) + '\n' + str(pointMin) + '\n' + str(pointMin) + '\n' + str(pointMax) + '\n' + str(pointStep) + '\n' + str(maxCor) + '\n' + str(0) + '\n' + str(0) + '\n' + str(0))
+                        cout.close()
+                        printLocation (text = 'Skipping metric ' + str(pmetrics[m]), x = PrinterCarriageNumber,y = 25)
+                        NNOptimizer(runTimeName = runTimeName, shortenProgress = shortenProgress, PrinterCarriageNumber = PrinterCarriageNumber, startupSleepTime = startupSleepTime, overideDirectory = overideDirectory)
+                        return False
 
                     if (abs(corAvg) > maxCor):    #saves argument settings if average coefficient is better than past max
                         maxCor = abs(corAvg)
@@ -344,17 +355,264 @@ def main():
     Main function; creates a multiprocessing pool of NNOptimizer functions.
     Sets up 3 proccess (4 CPU cores - 1 for OS/other overhead)
     '''
+    if __name__ == '__main__':
+    	pool = mp.Pool(processes = 3)#ChangeMe!!!
+    
+    	#pool.apply_async(NNOptimizer, [], dict(runTimeName = 'Part1', shortenProgress = True, PrinterCarriageNumber = 0, startupSleepTime = 0, overideDirectory = 'Data/Part1/'))
+        #pool.apply(NNOptimizer, [], dict(runTimeName = 'Part2', shortenProgress = True, PrinterCarriageNumber = 35, startupSleepTime = 1, overideDirectory = 'Data/Part2/')) #delete me l8r
+    	#pool.apply_async(NNOptimizer, [], dict(runTimeName = 'Part2', shortenProgress = True, PrinterCarriageNumber = 35, startupSleepTime = 1, overideDirectory = 'Data/Part2/'))
+    	
+        pool.apply(NNOptimizer, [], dict(runTimeName = 'Part3', shortenProgress = True, PrinterCarriageNumber = 70, startupSleepTime = 2, overideDirectory = 'Data/Part3/'))
+    
+    	print('\n')
 
-    pool = mp.Pool(processes = 3)
+
     
-    pool.apply_async(NNOptimizer, [], dict(runTimeName = 'Part1', shortenProgress = True, PrinterCarriageNumber = 0, startupSleepTime = 0, overideDirectory = 'Data/Part1/'))
-    pool.apply_async(NNOptimizer, [], dict(runTimeName = 'Part2', shortenProgress = True, PrinterCarriageNumber = 35, startupSleepTime = 1, overideDirectory = 'Data/Part2/'))
-    pool.apply(NNOptimizer, [], dict(runTimeName = 'Part3', shortenProgress = True, PrinterCarriageNumber = 70, startupSleepTime = 2, overideDirectory = 'Data/Part3/'))
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+#Let it be known that joey is a ghetto mother f@cker
+
+def GenerateNearestData(ticker,bs, fields = None, daterange = None, OutputMultiple = False, includeOptimalDates = False, overideDirectory = False):
+    '''
+    Generates an array of network data points for calculating distance and desired outputs for specified stock for all days.
+    The output list has 2 elements; out = [array of reference data points, array of desired outputs]
+
+    bs tells the function whether you want the outputs to be desired buy values ('b'), or desired sell values ('s'), or the profitspeed ('ps')
+
+    Fields is an optional list of strings specifying which data columns you want included. Defaults to all fields
+        possible fields-> ['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close', '2 Day Slope', '5 Day Slope', 'Standard Dev', '2 Day Momentum', '5 Day Momentum', '2D Discrete Moementum', '5D Discrete Moementum']
+        
+    Daterange is an optional 2 element list, containing the min and max dates desired for the data.
+    Dates must have following format:  'YYYY-MM-DD'
+    Dates must be given in increasing order (ie. 2012 before 2015)
+
+    OutputMultiple multiplies the desired output by specified amount. Defaults to 1. Used to provide larger output space
+
+    Setting includeOptimalDates to True adds a 3rd element to the output list: a column of trinary optimal dates.
+    Useful for accurate historical testing.
+
+    overideDirectory is an optional string to specify which directory to source the data from
+    '''
+    if overideDirectory:
+        filepath = overideDirectory + ticker
+    else:
+        filepath = 'Data/PcsData/' + ticker 
     
-    print('\n')
+    try:
+        sfile = open(filepath,'r')
+        row = 0
+        
+        data = np.genfromtxt(filepath,delimiter=',')    #generates data array from file
+        #sfile.close() 
+        data = np.delete(data,0,0)
+        data = np.delete(data,0,1)
+        
+        for line in sfile:       #obtains vertical array of dates
+            if row == 0:
+                row += 1
+            else:
+                r = line.split(',')
+               
+                for i in range(1,7,1):
+                    r[i] = float(r[i].rstrip())
+                
+                if row == 1:
+                    dates = np.array(r[0])
+                    row += 1
+                else:
+                    arow = np.array(r[0])  
+                    dates = np.vstack((dates,arow))
+        
+        sfile.close()            
+            
+        k = int(data.shape[0])      #number of dates in file
+        
+        desire = data[:,[10,11,12]]      #obtains desired outputs
+        optimals = data[:,9]        #trinary optimal dates
+
+        if (bs == 'b'):
+            desire = desire[:,0]
+        if (bs == 's'):
+            desire = desire[:,1]
+        if (bs == 'ps'):
+            desire = desire[:,2]
+        if OutputMultiple:
+            desire = desire * OutputMultiple
+
+        
+        if fields:      #truncates data array based on specified fields to include. Defaults to all
+            include = []
+            for f in fields:
+                if f == 'Open':
+                    include.append(0)
+                if f == 'High':
+                    include.append(1)
+                if f == 'Low':
+                    include.append(2)
+                if f == 'Close':
+                    include.append(3)
+                if f == 'Volume':
+                    include.append(4)
+                if f == 'Adj Close':
+                    include.append(5)
+                if f == '2 Day Slope':
+                    include.append(6)
+                if f == '5 Day Slope':
+                    include.append(7)
+                if f == 'Standard Dev':
+                    include.append(8)
+                if f == '2 Day Momentum':
+                    include.append(13)
+                if f == '5 Day Momentum':
+                    include.append(14)
+                if f == '2D Discrete Moementum':
+                    include.append(15)
+                if f == '5D Discrete Moementum':
+                    include.append(16)
+
+            ndata = data[:,include]
+        else:
+            ndata = data[:,[0,1,2,3,4,5,6,7,8,13,14,15,16]]
+         
+		
+        error = False
+        if daterange:       #if date range is specified
+            if (len(daterange[0]) != 10) or (len(daterange[1]) != 10) or (len(daterange) != 2):     #if daterange is invalid
+                print ('Error: Invalid Daterange')
+                error = True
+            else:
+                dateRangeList = []      #list containing range of dates
+                for d in daterange:
+                    date = []
+                    year = int(d[0:4])
+                    month = int(d[5:7])
+                    day = int(d[8:10])
+                    date.append(year)
+                    date.append(month)
+                    date.append(day)
+                    dateRangeList.append(date)
+                    
+        if error == False:
+            if daterange:       #truncates IO for that stock based on date range (if specified)
+                startIndx = 0   #first index in the array where date range is valid
+                for i in range(0,k,1):
+                    delete = False
+                    sdate = dates[i][0][1:11]
+                    syear = int(sdate[0:4])
+                    smonth = int(sdate[5:7])
+                    sday = int(sdate[8:10])
+                    if (syear > dateRangeList[1][0]):
+                        delete = True
+                    if (syear == dateRangeList[1][0] and smonth > dateRangeList[1][1]):
+                        delete = True
+                    if (syear == dateRangeList[1][0] and smonth == dateRangeList[1][1] and sday > dateRangeList[1][2]):
+                        delete = True
+                    
+                    if delete == True:
+                        startIndx += 1
+                    else:
+                        break
+
+                endIndx = k-1   #last index in the array where date range is valid
+                for i in range(k-1,1,-1):
+                    delete = False
+                    sdate = dates[i][0][1:11]
+                    syear = int(sdate[0:4])
+                    smonth = int(sdate[5:7])
+                    sday = int(sdate[8:10])
+                    if (syear < dateRangeList[0][0]):
+                        delete = True
+                    if (syear == dateRangeList[0][0] and smonth < dateRangeList[0][1]):
+                        delete = True
+                    if (syear == dateRangeList[0][0] and smonth == dateRangeList[0][1] and sday < dateRangeList[0][2]):
+                        delete = True
+                    
+                    if delete == True:
+                        endIndx -= 1
+                    else:
+                        break
+
+                #truncates output based on date range
+                dates = dates[startIndx:endIndx,:]
+                ndata = ndata[startIndx:endIndx,:]
+                desire = desire[startIndx:endIndx,:]
+                optimals = optimals[startIndx:endIndx,:]
+                k = int(dates.shape[0])
+
+        tout = [ndata , desire]
+        if includeOptimalDates:
+            tout.append(optimals)
+        return tout
+    
+    except Exception as e:
+        #sfile.close()
+        print(e)
+        print (ticker)
+        return 0
+
+    sfile.close()
+
+
+def NearestPredict(sourcePoint, dataPoints, outputs, points = 5, distanceMetric = 'seuclidean', dataPointWeights = False):
+    '''
+    Outputs a single predicted value using Nearest Neighbor. Each data point's contribution
+    to the prediction is inversely proportional to it's distance from the source point.
+
+    sourcePoint is the data vector of the day you want to predict.
+    dataPoints is an array of historical data vectors.
+    outputs is an array of historical outputs, corresponding with the dataPoints
+
+    points is how many of the closest dataPoints to include while calculating the prediction. Defaults to 5.
+
+    distanceMetric is the type of distance metric used in calculating distances (no duh). For more info,
+        please see cdist documentation: 
+        https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.spatial.distance.cdist.html#scipy.spatial.distance.cdist
+
+    dataPointWeights is not yet implemented
+    '''
+
+    ko = int(outputs.shape[0])
+    kd = int(dataPoints.shape[0])
+    wd = int(dataPoints.shape[1])
+    ws = int(sourcePoint.shape[1])
+
+    #Checks to make sure that the dimensions of all input arrays match
+    try:
+        if (wd != ws):
+            print('Error: SourcePoint of dimension ' + str(ws) + ' does not match dataPoints of dimension ' + str(wd))
+        elif (ko != kd):
+            print('Error: dataPoints of dimension ' + str(kd) + ' does not match outputs of dimension ' + str(ko))
+        else:
+            distanceArray = np.transpose(spatial.distance.cdist(sourcePoint,dataPoints,distanceMetric))     #calculate distances from source to points
+            
+            indxArray = np.zeros((ko,1))
+            for i in range(0,ko,1):
+                indxArray[i,0] = i 
+
+            distanceArray = np.hstack((distanceArray,indxArray))
+            distanceArray = distanceArray[distanceArray[:,0].argsort()]     #sorts indexes by distance
+
+            #takes the weighted average of outputs, treating their distances as experimental uncertainties
+            uncertaintySum = 0
+            measurementSum = 0
+            for p in range(0,points,1):
+                uncertaintySum += 1/((distanceArray[p,0])**2)
+                measurementSum += (outputs[int(distanceArray[p,1])])/((distanceArray[p,0])**2)
+
+
+            prediction = measurementSum / uncertaintySum
+            if ((prediction < 0) or (prediction > 0) or (prediction == 0)):
+                return prediction
+            else:
+                return 0
+
+    except Exception as e:
+        #print (e + '\n')
+        return 0
 
 
 #----------------------------------------------------------------------------------------------------------------------------
 
-
 main()
+#NNOptimizer(runTimeName = 'Part3', shortenProgress = False, PrinterCarriageNumber = 70, startupSleepTime = 2, overideDirectory = 'Data/Part3/');
