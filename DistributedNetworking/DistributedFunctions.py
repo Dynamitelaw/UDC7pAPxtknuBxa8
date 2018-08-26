2ed623ca27cc5a762fa646184a72d9b7d843d200
Dynamitelaw <noobeater122333444455555@gmail.com>
2018-08-25 23:24:08 -0400

    Added chatapp.py for testing

---
+++ DistributedNetworking/DistributedFunctions.py
@@ -0,0 +1,15 @@
+'''
+Dynamitelaw
+
+Contains the functions that are called by incomming bot commands
+'''
+
+#External imports
+import os
+
+
+def updateCodebase():
+    '''
+    Updates the codebase by doing a git pull.
+    Kills current thread
+    '''
